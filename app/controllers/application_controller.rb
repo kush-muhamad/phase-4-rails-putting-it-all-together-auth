7194a6f7eb426455bbf83297c99c8bab0edd8ad6
kush Muhamad <kushmohammed98@yahoo.com>
2023-07-02 15:42:14 +0300

    completed task

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+before_action :authorize
+
+private
+
+def authorize
+  @current_user = User.find_by(id: session[:user_id])
+  render json: { errors: ["Not authorized"] }, status: 401 unless @current_user
+end
+
+def render_unprocessable_entity_response(inavlid)
+  render json: { errors: inavlid.record.errors.full_messages }, status: 422
+
+end
 
 end
